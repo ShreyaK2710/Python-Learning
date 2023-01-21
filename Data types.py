ef4dcdcb7c9df2fb8af8437ec244a243a1290a48
Shreya K <95535050+ShreyaK2710@users.noreply.github.com>
2023-01-21 17:07:07 +0530

    Create Data types.py

---
+++ Data types.py
@@ -0,0 +1,12 @@
+string="hello"
+integers=10
+floats=10.01
+list=["hey","hi","hello"] #mutable
+tuple=("hey","hi","hello") #immutable
+sets={"hey","hi","hello"} #order changes when executed
+dictionary={
+  "key"="value1",
+  "key2"="value2",
+}
+boolean=True #or False
+none=None
